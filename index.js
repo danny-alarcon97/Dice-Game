70cbc469b6c4c9f65f8a29ec6b84f3f653a70dfc
daniel <danny.alarcon1997@gmail.com>
2022-10-14 15:37:37 -0400

    finishing JS DOM commands

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //Random # between 1 - 6
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomDiceImg = `dice${randomNumber1}.jpg`; // dice1.jpg - dice2.jpg
+var randomImgSrc = `img/dice${randomNumber1}.jpg`; // img/dice1.jpg - img/dice6.jpg
+var randomImgSrc2 = `img/dice${randomNumber2}.jpg`;
 
-var randomImgSrc = `img/randomDiceImg`; // img/dice1.jpg - img/dice6.jpg
+var image1 = document.getElementById("img1").setAttribute("src", randomImgSrc);
+var image2 = document.getElementById("img2").setAttribute("src", randomImgSrc2);
 
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImgSrc);
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins";
+} else {
+  document.querySelector("h1").innerHTML = "It's a Tie!";
+}
