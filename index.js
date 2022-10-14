89e5e0599d8321aeba63e49e328d0a05b533c6c5
daniel <danny.alarcon1997@gmail.com>
2022-10-14 14:41:33 -0400

    changing img names

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //Random # between 1 - 6
+
+var randomDiceImg = `dice${randomNumber1}.jpg`; // dice1.jpg - dice2.jpg
+
+var randomImgSrc = `img/randomDiceImg`; // img/dice1.jpg - img/dice6.jpg
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImgSrc);
